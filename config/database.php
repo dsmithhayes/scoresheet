4f6f4d4704e4ae8d141ea9d80ef794b73537134c
Dave Smith-Hayes <dsmithhayes@gmail.com>
2015-11-22 15:53:54 -0500

    Added the per-9 calculations for the pitching.

--- config/database.php
+++ config/database.php
@@ -5,5 +5,18 @@
  */
 
 return [
-
+    'Datasources' => [
+        'default' => [
+            'className' => 'Cake\Database\Connection',
+            'driver' => 'Cake\Database\Driver\Mysql',
+            'persistent' => false,
+            'host' => 'localhost',
+//            'username' => 'my_app',
+//            'password' => 'sekret',
+            'database' => 'scoresheet.db',
+            'encoding' => 'utf8',
+            'timezone' => 'UTC',
+            'cacheMetadata' => true,
+        ]
+    ]
 ];
--- src/Statistics/Fielding/PitchingCalculator.php
+++ src/Statistics/Fielding/PitchingCalculator.php
@@ -12,15 +12,6 @@ use Scoresheet\Statistics\Game;
 class PitchingCalculator extends Calculator
 {
     /**
-     * @param  int   $bb The number of base on balls
-     * @return float     The calculcated base on balls per 9 innings
-     */
-    public static function bb9($bb, $ip)
-    {
-        return round((($bb * Game::TOTAL_INNINGS) / $ip), 1);
-    }
-    
-    /**
      * @param  int   $er The number of earned runs
      * @param  int   $ip The number of innings pitched
      * @return float     The calculated ERA.
@@ -31,13 +22,55 @@ class PitchingCalculator extends Calculator
     }
     
     /**
-     * @param int $bb The number of base on balls
-     * @param int $h  The number of hits
-     * @param int $ip The number of innings pitched
-     * @return float The calculated walks and hits per inning pitched.
+     * @param  int   $bb The number of base on balls
+     * @param  int   $h  The number of hits
+     * @param  int   $ip The number of innings pitched
+     * @return float     The calculated walks and hits per inning pitched.
      */
     public static function whip($bb, $h, $ip)
     {
         return round((($bb + $h) / $ip), 3);
     }
+
+    /**
+     * @param  int   $h  The number of hits
+     * @param  int   $ip The number of innings pitched
+     * @return float     The calculated hits per 9 innings
+     */
+    public static function h9($h, $ip)
+    {
+        return round((($h * Game::TOTAL_INNINGS) / $ip), 1);
+    }
+    
+    /**
+     * @param  int   $hr The number of home runs
+     * @param  int   $ip The number of innings pitched
+     * @return float     The claculated home runs per 9 innings
+     */
+    public static function hr9($hr, $ip)
+    {
+        return round((($hr * Game::TOTAL_INNINGS) / $ip), 1);
+    }
+
+    /**
+     * @param  int   $bb The number of base on balls
+     * @param  int   $ip The number of innings pitched
+     * @return float     The calculcated base on balls per 9 innings
+     */
+    public static function bb9($bb, $ip)
+    {
+        return round((($bb * Game::TOTAL_INNINGS) / $ip), 1);
+    }
+    
+    /**
+     * @param  int   $so The number of strike outs
+     * @param  int   $ip The number of innings pitched
+     * @return float     The claculated strike outs per 9 innings
+     */
+    public static function so9($so, $ip)
+    {
+        return round((($so *Game::TOTAL_INNINGS) / $ip), 1);
+    }
+    
+    
 }
