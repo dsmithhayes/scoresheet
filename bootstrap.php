3a47cc87ee4827c828b53fee26d469fbf2413f4e
Dave Smith-Hayes <dsmithhayes@gmail.com>
2015-11-22 21:54:43 -0500

    Fixed the bootstrap to get rid of the cake ORM.

--- bootstrap.php
+++ bootstrap.php
@@ -7,23 +7,3 @@ require 'vendor/autoload.php';
  * all of the configure and make sure all of the namespaces are
  * available to the front controller.
  */
-
-/**
- * Namespaces
- */
-use Cake\Datasource\ConnectionManager;
-
-/**
- * Connect to a database
- */
-
-ConnectionManager::config('default', [
-    'className'     => 'Cake\Database\Connection',
-    'driver'        => 'Cake\Database\Driver\Sqlite',
-    'persistent'    => false,
-    'host'          => 'localhost',
-    'database'      => 'scoresheet.db',
-    'encoding'      => 'utf8',
-    'timezone'      => 'UTC',
-    'cacheMetadata' => true,
-]);
